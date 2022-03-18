d4bc879810e3dedb0f08eabd6cbdbc4857a6f99f
Kyle Holzinger <kylelholzinger@gmail.com>
2022-03-17 20:19:16 -0400

    [KH] add gtm

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,56 +1,57 @@
-import React from 'react';
-import { Link, graphql } from 'gatsby';
+import React from 'react'
+import { Link, graphql } from 'gatsby'
 
-import Bio from '../components/bio';
-import Layout from '../components/layout';
-import SEO from '../components/seo';
-import Splash from '../components/splash';
-import { rhythm } from '../utils/typography';
+import Bio from '../components/bio'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import Splash from '../components/splash'
+import { rhythm } from '../utils/typography'
+import '../components/gtm'
 
 const BlogIndex = ({ data, location }) => {
-	const siteTitle = data.site.siteMetadata.title;
-	const posts = data.allMarkdownRemark.edges;
+  const siteTitle = data.site.siteMetadata.title
+  const posts = data.allMarkdownRemark.edges
 
-	return (
-		<>
-			<Splash />
+  return (
+    <>
+      <Splash />
 
-			<Layout location={location} title={siteTitle}>
-				<SEO title="Kyle Holzinger" />
-				<Bio />
-				{posts.map(({ node }) => {
-					const title = node.frontmatter.title || node.fields.slug;
-					return (
-						<article key={node.fields.slug}>
-							<header>
-								<h3
-									style={{
-										marginBottom: rhythm(1 / 4),
-                    fontFamily: 'Financier'
-									}}
-								>
-									<Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-										{title}
-									</Link>
-								</h3>
-								<small>{node.frontmatter.date}</small>
-							</header>
-							<section>
-								<p
-									dangerouslySetInnerHTML={{
-										__html: node.frontmatter.description || node.excerpt,
-									}}
-								/>
-							</section>
-						</article>
-					);
-				})}
-			</Layout>
-		</>
-	);
-};
+      <Layout location={location} title={siteTitle}>
+        <SEO title="Kyle Holzinger" />
+        <Bio />
+        {posts.map(({ node }) => {
+          const title = node.frontmatter.title || node.fields.slug
+          return (
+            <article key={node.fields.slug}>
+              <header>
+                <h3
+                  style={{
+                    marginBottom: rhythm(1 / 4),
+                    fontFamily: 'Financier',
+                  }}
+                >
+                  <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+                    {title}
+                  </Link>
+                </h3>
+                <small>{node.frontmatter.date}</small>
+              </header>
+              <section>
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: node.frontmatter.description || node.excerpt,
+                  }}
+                />
+              </section>
+            </article>
+          )
+        })}
+      </Layout>
+    </>
+  )
+}
 
-export default BlogIndex;
+export default BlogIndex
 
 export const pageQuery = graphql`
   query {
@@ -75,4 +76,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`;
+`
