05bea77dbe30d814402cfa62330744a9c3a636ed
Kyle Holzinger <kylelholzinger@gmail.com>
2022-10-18 21:52:12 -1000

    [KH] site redesign

--- src/utils/useMounted.js
+++
@@ -1,9 +0,0 @@
-import { useEffect, useState } from 'react';
-
-export default function useMounted() {
-	const [isMounted, setMounted] = useState(false);
-
-	useEffect(() => setMounted(true), []);
-
-	return isMounted;
-}
